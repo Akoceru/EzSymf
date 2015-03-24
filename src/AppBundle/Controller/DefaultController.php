0526e50bc332054c63275a8dcbefdaaeb3967a3a
Akoceru <axl.78@hotmail.fr>
2015-03-24 20:32:29 +0100

    EZsymf sua avance

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,14 +4,54 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/app/example", name="homepage")
      */
-    public function indexAction()
+    public function indexAction(Request $r)
     {
+        var_dump($r->server);die;
         return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/hello/{name}", name="hello_world")
+     */
+    public function helloAction($name)
+    {
+        return $this->render('AppBundle::hello-world.html.twig', [
+            'name'=> $name
+
+        ]);
+    }
+
+    /** * @Route("/{year}-{month}-{day}-{name}/{page}",
+        * requirements={ * "year"="\d{4}",
+        * "month"="\d{2}", * "day"="\d{2}",
+        * "page"="\d+"
+        * },
+        * defaults={ * "page"="1"
+        * },
+        * name="article"
+        * )
+     */
+    public function articledAction($year,$month,$day,$name,$page)
+    {
+        return new Response('Article page ' .$year);
+    }
+
+    /**
+     * @Route("/twig", name="twig")
+     */
+    public function twigAction()
+    {
+        return $this->render("AppBundle::twig.html.twig", [
+            "content" => "<h1>loool</h1>",
+        ]);
     }
 }
